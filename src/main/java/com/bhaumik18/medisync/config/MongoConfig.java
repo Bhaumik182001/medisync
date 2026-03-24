7a95b01b113052ee833f13f7a6c725958501a16c
Bhaumik <bhaumik182001@gmail.com>
2026-03-24 20:10:27 +0530

    feat(identity): complete milestone 1.1 - initialize Spring Boot 4 foundation, MongoDB polyglot schema, and local Docker infrastructure

---
+++ src/main/java/com/bhaumik18/medisync/config/MongoConfig.java
@@ -0,0 +1,10 @@
+package com.bhaumik18.medisync.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+
+}
